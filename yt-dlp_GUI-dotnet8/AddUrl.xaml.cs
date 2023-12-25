a6f70263908dc628d54dbac2e230f06c965fe18a
toarugakusei <toarugakuseikaizou@gmail.com>
2023-12-25 22:46:45 +0900

    AddURLを追加

---
+++ yt-dlp_GUI-dotnet8/AddUrl.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace yt_dlp_GUI_dotnet8
+{
+    /// <summary>
+    /// AddUrl.xaml の相互作用ロジック
+    /// </summary>
+    public partial class AddUrl : Window
+    {
+        public AddUrl()
+        {
+            InitializeComponent();
+        }
+        public string[] urls;
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            string tmp = Box.Text;
+            urls = tmp.Split("\r\n");
+            this.Close();
+        }
+    }
+}
--- yt-dlp_GUI-dotnet8/MainWindow.xaml.cs
+++ yt-dlp_GUI-dotnet8/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 using YoutubeDLSharp;
 using YoutubeDLSharp.Metadata;
 
@@ -23,6 +24,7 @@ namespace yt_dlp_GUI_dotnet8
             InitializeAsync();
             QuestionDownloadFirst();
             progress = new Progress<DownloadProgress>((p) => showProgress(p));
+
         }
         /// <summary>
         /// ここでWebView関連の設定を行っている
@@ -36,44 +38,73 @@ namespace yt_dlp_GUI_dotnet8
             //新しいタブで開かないようにする。
             webview.CoreWebView2.NewWindowRequested += NewWindowRequested;
             webview.CoreWebView2.SourceChanged += CompletedPage;
-
+            SizeChanged += MainWindow_SizeChanged;
 
         }
+
+        private void MainWindow_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            StackPanel_Info.MaxHeight = this.Height;
+        }
+
         //
         private VideoData video;
         FormatData[] formats;
-
-        private async void DownloadAsync()
+        public string folder ="none";
+        public bool cancel = false;
+        private void DownloadAsync(string url)
         {
             var ytdl = new YoutubeDL();
             ytdl.YoutubeDLPath = @".\yt-dlp.exe";
             ytdl.FFmpegPath = @".\ffmpeg.exe";
-            var dlg = new CommonOpenFileDialog();
-            dlg.IsFolderPicker = true;
-            if (dlg.ShowDialog() == CommonFileDialogResult.Ok)
+            if (folder == "none")
             {
-                ytdl.OutputFolder = dlg.FileName;
+                var dlg = new CommonOpenFileDialog();
+                dlg.IsFolderPicker = true;
+                if (dlg.ShowDialog() == CommonFileDialogResult.Ok)
+                {
+                    ytdl.OutputFolder = dlg.FileName;
+                    folder = dlg.FileName;
+                    DownloadVideo(url, ytdl);
+                }
+                else
+                {
+                    cancel = true;
+                    MessageBox.Show("キャンセルされました。","情報",MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+
+
+            }
+            else
+            {
+                ytdl.OutputFolder = folder;
+
+                DownloadVideo(url, ytdl);
             }
 
-            await Task.Run(() =>
+        }
+
+        private void DownloadVideo(string url, YoutubeDL ytdl)
+        {
+            var run = Task.Run(() =>
             {
                 var cts = new CancellationTokenSource();
                 this.Dispatcher.Invoke((Action)(() =>
                 {
-                    ytdl.RunVideoDownload(webview.CoreWebView2.Source,
+                    ytdl.RunVideoDownload(url,
                                progress: progress, ct: cts.Token);
-                    /*var res = await ytdl.RunVideoDataFetch("https://www.youtube.com/watch?v=OSHNSq0rA3s");
-                    // get some video information
-                    video = res.Data;
-                    Debug.WriteLine($"video={video}\ntitle={title}\nuploader={uploader}\nviews={views}\nformats={formats.ToString()}");
-                    */
+
                 }));
                 /*foreach( var format in formats )
                 {
                     Debug.WriteLine(format);
                 }*/
-                
+
             });
+            if (run.Status == TaskStatus.Running)
+            {
+                run.Wait();
+            }
         }
 
         private async void QuestionDownloadFirst()
@@ -110,11 +141,36 @@ namespace yt_dlp_GUI_dotnet8
 
 
         }
+        int count = 0;
         private void showProgress(DownloadProgress p)
         {
             txtState.Content = p.State.ToString();
             prog.Value = p.Progress;
-            progText.Content = $"speed: {p.DownloadSpeed} | left: {p.ETA}";
+            int a = (int)(p.Progress * 100);
+            progText.Content = $"speed: {p.DownloadSpeed} | left: {p.ETA} | %: {a}%";
+            if (p.State.ToString() == "Success")
+            {
+                count += 1;
+                Debug.WriteLine($"Count::{count}");
+            }
+            else if(p.State.ToString() == "Error")
+            {
+                MessageBox.Show("失敗");
+            }
+            if(count == list.Items.Count)
+            {
+                var done = MessageBox.Show("All Done!","情報",MessageBoxButton.OK, MessageBoxImage.Information);
+                if(done == MessageBoxResult.OK)
+                {
+                    list.ClearValue(ItemsControl.ItemsSourceProperty); 
+                    folder = "none";
+                    count = 0;
+                    progText.Content = "Download States";
+                    txtState.Content = "States";
+
+                }
+
+            }
         }
 
         private void NewWindowRequested(object sender, CoreWebView2NewWindowRequestedEventArgs e)
@@ -188,7 +244,7 @@ namespace yt_dlp_GUI_dotnet8
 
         private void download_Click(object sender, RoutedEventArgs e)
         {
-            DownloadAsync();
+            //DownloadAsync();
         }
         ObservableCollection<DLList> dLLists = new ObservableCollection<DLList>();
         private void download_Click_1(object sender, RoutedEventArgs e)
@@ -218,9 +274,48 @@ namespace yt_dlp_GUI_dotnet8
         {
             dLLists.Clear();
         }
+        private void Add_Url_List(object sender, RoutedEventArgs e)
+        {
+            AddUrl addURl = new AddUrl();
+            addURl.ShowDialog();
+            var urls = addURl.urls;
+            if (urls != null)
+            {
+                foreach (var url in urls)
+                {
+                    dLLists.Add(new DLList { url = url });
+                }
+                list.ItemsSource = dLLists;
+            }
+            else
+            {
+                MessageBox.Show("使用できない文字列が入っているか、値が無効です。", "警告", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+        }
         private void Start_Click(object sender, RoutedEventArgs e)
         {
-            DownloadAsync();
+            if (list.Items.Count == 0)
+            {
+                MessageBox.Show("URLを追加してください", "警告", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                foreach (var r in list.Items)
+                {
+                    if (cancel == true)
+                    {
+                        cancel = false;
+                        folder = "none";
+                        break;
+
+                    }
+                    var a = (r as DLList)?.url;
+                    Debug.WriteLine(a);
+                    DownloadAsync(a);
+
+                }
+            }
         }
 
         private void SetPixel_Checked(object sender, RoutedEventArgs e)
@@ -241,6 +336,46 @@ namespace yt_dlp_GUI_dotnet8
         private void prog_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             Debug.WriteLine(prog.Value.ToString());
+        }
+        private async void Info_Search(object sender, RoutedEventArgs e)
+        {
+            var ytdl = new YoutubeDL();
+            ytdl.YoutubeDLPath = @".\yt-dlp.exe";
+            ytdl.FFmpegPath = @".\ffmpeg.exe";
+            if (SearchBox_Info.Text != "" && SearchBox_Info.Text.Contains("https://"))
+            {
+                try
+                {
+                    var res = await ytdl.RunVideoDataFetch(SearchBox_Info.Text);
+                    // get some video information
+                    video = res.Data;
+                    BitmapImage bti = new BitmapImage(new Uri(video.Thumbnail));
+                    thumbPic.Source = bti;
+                    VideoTitle.Header = video.Title;
+                    VidInfo.Text = video.ToString();
+
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("何らかのエラーにより、処理ができませんでした。\nURLが正しいかご確認ください。");
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("URLを入力してください!", "注意", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+        }
+
+        private void CodecToggle_Checked(object sender, RoutedEventArgs e)
+        {
+            codec.IsEnabled = true;
+        }
+
+        private void CodecToggle_Unchecked(object sender, RoutedEventArgs e)
+        {
+            codec.IsEnabled = false;
         }
     }
 }
