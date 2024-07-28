fdee3570e668ba70d19dc4c6d5b41ebae5de8a9a
ToaRuGakusei <users.noreply.github.com>
2024-07-28 17:25:40 +0900

    アップデートチェック機構の日付を変更

--- yt-dlp_GUI-dotnet8/Tool/CheckUpdate.cs
+++ yt-dlp_GUI-dotnet8/Tool/CheckUpdate.cs
@@ -36,9 +36,9 @@ namespace yt_dlp_GUI_dotnet8.Tool
                 Debug.WriteLine(latestVersion.DateTime.AddHours(9));
 
                 //JSTで日付を管理（GitHub側は世界標準時間になっているから、9時間プラスして日本時間に変更）
-                if (repo == "yt-dlp_GUI-dotnet8" && latestVersion.DateTime.AddHours(9) > Convert.ToDateTime("2024/04/24 10:10:00"))
+                if (repo == "yt-dlp_GUI-dotnet8" && latestVersion.DateTime.AddHours(9) > Convert.ToDateTime("2024/07/28 18:10:00"))
                 {
-                    var update = MessageBox.Show($"新しいYT-DLPのバージョン({latestRelease.TagName})が見つかりました。\nアップデートしますか？", "お知らせ", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                    var update = MessageBox.Show($"新しいバージョン({latestRelease.TagName})が見つかりました。\nアップデートしますか？", "お知らせ", MessageBoxButton.YesNo, MessageBoxImage.Question);
                     if (update != MessageBoxResult.Yes)
                     {
                         return;
@@ -56,7 +56,7 @@ namespace yt_dlp_GUI_dotnet8.Tool
                 }
                 else if(repo == "yt-dlp" && latestVersion.DateTime.AddHours(9) > Convert.ToDateTime(lastUpdate))
                 {
-                    var update = MessageBox.Show($"新しいバージョン({latestRelease.TagName})が見つかりました。\nアップデートしますか？", "お知らせ", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                    var update = MessageBox.Show($"新しいYT-DLPのバージョン({latestRelease.TagName})が見つかりました。\nアップデートしますか？", "お知らせ", MessageBoxButton.YesNo, MessageBoxImage.Question);
                     if (update != MessageBoxResult.Yes)
                     {
                         return;
